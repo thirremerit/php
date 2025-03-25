94604f4020992395c5dd7771d0bf0415c8d290be
thirremerit <eritgovori11@gmail.com>
2025-03-25 20:41:03 +0100

    lesson 7

---
+++ lesson7/conection.php
@@ -0,0 +1,15 @@
+<?php
+$host= 'localhost';
+$user= 'root';
+$pass='';
+
+try{
+$conn = new PDO("mysql:host=$host",$user,$pass);
+echo "connected";
+
+}catch(Exception $e){
+    echo "not connected";
+}
+
+
+?>
