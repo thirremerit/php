28344a2830b2cdd589b6bf3218422080827646fb
thirremerit <eritgovori11@gmail.com>
2025-04-15 20:45:10 +0200

    day 11

---
+++ day 11/delete.php
@@ -0,0 +1,18 @@
+<?php
+include_once("config.php");
+
+$id=$_GET['id'];
+
+$sql ="DELETE FROM users  where id =:id";
+
+
+$getUsers = $conn->prepare($sql);
+
+
+$getUsers->bindParam(':id',$id);
+
+$getUsers->execute();
+
+header("location:dashboard.php");
+
+?>
